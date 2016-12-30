56692ea110524bf0a8e05621c46b2b4afff7de1a
juliakimchung <julhkm@gmail.com>
2016-12-30 16:16:22 -0600

    fixed damages points

--- js/battle.js
+++ js/battle.js
@@ -9,26 +9,30 @@ let $output = $("#output");
 $("#card").show();
 
 let robotBattle = (pickOne, pickTwo) => {
-	//console.log("test",robot.DroneOne );
+	console.log("test",robot.DroneOne );
 	let test = new robot.DroneOne();
 	console.log("test",test );
 	console.log("damageTest",test.damage );
-	//console.log("pickOne", pickOne );
+	console.log("pickOne", pickOne );
 	console.log("pickOne[0]",pickOne[0] );
 	
 	let toy = robot[pickOne[1]];
 	let newToy = new robot[pickOne[1]]();
 
-	console.log("toy",newToy.damage );
+	console.log("newToy",newToy );
 	let anotherToy = new robot[pickTwo[1]]();
 	let realDamage1 = newToy.health - anotherToy.damage;
-	let realDamage2 = anotherToy.health - newToy.health;
+	console.log("newToy.health",newToy.health );
+	console.log("newToy.damage",newToy.damage );
+	console.log("anotherToy.health",anotherToy.health );
+	console.log("anotherToy.damage",anotherToy.damage );
+	let realDamage2 = anotherToy.health - newToy.damage;
 	console.log("realDamage1", realDamage1);
 	console.log("realDamage2", realDamage2 );
 
-if(newToy.damage > anotherToy.damage || anotherToy.damage === 0) {
+if(newToy.damage > anotherToy.damage || anotherToy.damage < 0) {
 		$output.html(`<p>${newToy.name} has Won by ${realDamage1} health points</p>` );
-	} else if (anotherToy.damage > newToy.damage || newToy.damage === 0) {
+	} else if (anotherToy.damage > newToy.damage || newToy.damage <0) {
 		$output.html(`<p>${anotherToy.name} has WON
 		by ${realDamage2} health points</p>`);
 	}
